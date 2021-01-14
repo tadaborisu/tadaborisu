4c47ea98a5adefc01309c54696f8b9214b0d3abf
Boris Ibragimov <44411568+tadaborisu@users.noreply.github.com>
2021-01-15 01:57:02 +0300

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,9 @@
+function copy() {
+    
+    var copyText = document.getElementById("myInput");
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+  
+    document.execCommand("copy");
+ }
